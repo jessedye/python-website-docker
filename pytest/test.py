ffea7fe061208f1053dca49fcb4f8b79ba4887f3
Jesse Dye <jesse.dye.ctr@us.af.mil>
2024-08-23 15:55:44 -0400

    updated

--- pytest/test.py
+++ pytest/test.py
@@ -1,4 +1,10 @@
 import pytest
+import sys
+import os
+
+# Add the directory containing server.py to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from server import app
 
 @pytest.fixture
