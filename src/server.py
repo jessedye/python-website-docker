2f1af997958a28319efb3c25c079b9c5c6507836
Jesse Dye <jesse.dye.ctr@us.af.mil>
2024-08-23 16:37:06 -0400

    added healthcheck

--- src/server.py
+++ src/server.py
@@ -1,11 +1,17 @@
 from flask import Flask  
-from flask import render_template
+from flask import render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/healthz/ready')
+def health_check_ready():
+    # Perform any necessary checks here (e.g., database connectivity)
+    # For simplicity, we'll just return a 200 OK response
+    return jsonify(status="ready"), 200
 
 # run the application
 if __name__ == "__main__":  
